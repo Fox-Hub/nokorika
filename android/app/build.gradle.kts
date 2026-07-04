a7abb3e864534f440c21b0d6c6c65435c3f674cd
w.k <tekno753@gmail.com>
2026-07-04 10:16:49 +0900

    ディレクトリ構成修正と通知周りの修正

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,12 +5,11 @@ kotlin {
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.busi_calendar_app"
+    namespace = "com.nokorikastudio.nokorika"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -25,10 +24,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.busi_calendar_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.nokorikastudio.nokorika"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -38,8 +34,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
--- busi_calendar_app/android/app/src/main/kotlin/com/example/busi_calendar_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.busi_calendar_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
